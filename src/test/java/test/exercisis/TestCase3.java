aebee19f7246c5f267ebb38804242a122d104a99
ibrahim Yazar <ibrahimyazarusa@gmail.com>
2019-12-13 11:05:02 -0500

    added new commit

--- src/test/java/test/exercisis/TestCase3.java
+++ src/test/java/test/exercisis/TestCase3.java
@@ -39,6 +39,7 @@ public class TestCase3 {
         //BrowserUtills.wait(2);
         wait.until(ExpectedConditions.visibilityOf(activities));
         wait.until(ExpectedConditions.elementToBeClickable(activities));
+        BrowserUtils.wait(2);
         activities.click();
         BrowserUtils.wait(2);
         WebElement ClndrAct = driver.findElement(By.linkText("Calendar Events"));
@@ -49,7 +50,7 @@ public class TestCase3 {
     }
 
 
-    @Test(description = "")
+    @Test(description = "verify that")
     public void Testcase3(){
 
         WebElement CreateCalenderEventButton = driver.findElement(By.cssSelector("[title='Create Calendar event']"));
@@ -92,7 +93,7 @@ public class TestCase3 {
     }
 
 
-    @Test
+    @Test(description = "Verify that difference between end and start time is exactly 1 hour")
     public void test5(){
 
         WebElement CreateCalenderEventButton = driver.findElement(By.cssSelector("[title='Create Calendar event']"));
@@ -104,22 +105,41 @@ public class TestCase3 {
         Time1.click();
         WebElement Starttime = driver.findElement(By.xpath("//li[contains(text(), '10:00 PM')]"));
         Starttime.click();
-        String StartTimetext = Starttime.getText();
-        System.out.println(StartTimetext);
+       // String StartTimetext = Starttime.getText();
+        //System.out.println(StartTimetext);
 
         String ExpectedTime = "11:00 PM";
         BrowserUtils.wait(2);
-        WebElement EndTime = driver.findElement(By.cssSelector("[class='input-small timepicker-input end ui-timepicker-input']"));
+        WebElement EndTime = driver.findElement(By.cssSelector("/html/body/div[8]/ul/li[3]"));
         EndTime.click();
         String text2 = EndTime.getText();
-
-
+// /html/body/div[8]/ul/li[3]
+        // [class='input-small timepicker-input end ui-timepicker-input']
         BrowserUtils.wait(3);
         Assert.assertEquals(text2, ExpectedTime);
 
 
     }
 
+    @Test(description = "Verify that start and end date input boxes are displayed")
+    public void test7(){
+
+        // 4. Click on “Create Calendar Event” button
+        WebElement CreateCalendarEvent = driver.findElement(By.cssSelector("[title='Create Calendar event']"));
+        CreateCalendarEvent.click();
+        // 5. Select “All-Day Event” checkbox
+        WebElement alldayEventCheckbox = driver.findElement(By.id("oro_calendar_event_form_allDay-uid-5df2f810d9005"));
+        alldayEventCheckbox.click();
+        // 6. Verify that “All-Day Event” checkbox is selected
+        Assert.assertTrue(alldayEventCheckbox.isSelected(), "it is not selected");
+        // Verify that start and end time input boxes are not displayed
+        WebElement time = driver.findElement(By.cssSelector("[placeholder='time']"));
+        Assert.assertTrue(!time.isDisplayed(), "start and end time input boxes are displayed");
+        // 8. Verify that start and end date input boxes are displayed
+        WebElement startAndEndDate = driver.findElement(By.cssSelector("[placeholder='Choose a date']"));
+        Assert.assertTrue(startAndEndDate.isDisplayed(), "start and end date input boxes are not displayed");
+
+    }
 
     @AfterMethod
     public void tearDown(){
